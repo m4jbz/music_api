ca345eaf5133b5642e1e3ae02a134c8a0170c88b
m4jbz <marcoajb05@proton.me>
2025-12-20 23:01:56 -0600

    the logic was separated. service.js does the bussines logic now

--- controllers/muController.js
+++ controllers/muController.js
@@ -1,19 +1,9 @@
-const { Artist, Album, Track, Genre } = require("../models/muSchema");
-const dotenv = require("dotenv");
-const validator = require("validator"); // for validating the url
-
-dotenv.config();
+const MuService = require("../services/muService")
 
 class MuController {
     static async getAllArtists(req, res) {
         try {
-            const { name, genre } = req.query;
-            const filter = {}
-
-            if (name) filter.name = name;
-            if (genre) filter.genres = genre;
-
-            const artists = await Artist.find(filter);
+            const artists = await MuService.getAllArtists(req.query);
             res.status(200).json(artists);
         } catch (err) {
             res.status(500).json({ message: err.message });
@@ -22,7 +12,7 @@ class MuController {
 
     static async getAllGenres(req, res) {
         try {
-            const genres = await Genre.find({});
+            const genres = await MuService.getAllGenres();
             res.status(200).json(genres);
         } catch (err) {
             res.status(500).json({ message: err.message });
@@ -31,13 +21,7 @@ class MuController {
 
     static async getAllAlbums(req, res) {
         try {
-            const { year, genre } = req.query;
-            const filter = {}
-
-            if (year) filter.recorded_date = year;
-            if (genre) filter.genres = genre;
-
-            const albums = await Album.find(filter);
+            const albums = await MuService.getAllAlbums(req.query);
             res.status(200).json(albums);
         } catch (err) {
             res.status(500).json({ message: err.message });
@@ -46,7 +30,7 @@ class MuController {
 
     static async getAllTracks(req, res) {
         try {
-            const tracks = await Track.find({});
+            const tracks = await MuService.getAllTracks();
             res.status(200).json(tracks);
         } catch (err) {
             res.status(500).json({ message: err.message });
@@ -55,9 +39,7 @@ class MuController {
 
     static async getAlbumByArtist(req, res) {
         try {
-            const { id } = req.params;
-            const artist = await Artist.findOne({ _id: id});
-            const album = await Album.findOne({ performer: artist.name });
+            const album = await MuService.getAlbumByArtist(req.params);
             res.status(200).json(album);
         } catch (err) {
             res.status(500).json({ message: err.message });
--- services/muService.js
+++ services/muService.js
@@ -1,92 +1,46 @@
 const { Artist, Album, Track, Genre } = require("../models/muSchema");
 
-// const saveArtists = async (artistsToSave) => {
-//     try {
-//         const totalAInsertar = artistsToSave.length;
-//         const docsInsertados = await Artist.insertMany(artistsToSave);
-//
-//         if (docsInsertados.length === totalAInsertar) {
-//             console.log(`Éxito total: Se insertaron los ${totalAInsertar} artistas.`);
-//         } else {
-//             console.warn(`Atención: Se enviaron ${totalAInsertar} pero solo se confirmaron ${docsInsertados.length}.`);
-//         }
-//
-//     } catch (error) {
-//         console.error("Error durante la inserción:");
-//         if (error.writeErrors) {
-//             console.error(`Se insertaron ${error.insertedDocs.length} documentos, pero hubo ${error.writeErrors.length} errores.`);
-//         } else {
-//             console.error(error.message);
-//         }
-//     }
-// };
-//
-// const saveAlbums = async (albumsToSave) => {
-//     try {
-//         const totalAInsertar = albumsToSave.length;
-//         const docsInsertados = await Album.insertMany(albumsToSave);
-//
-//         if (docsInsertados.length === totalAInsertar) {
-//             console.log(`Éxito total: Se insertaron los ${totalAInsertar} albums.`);
-//         } else {
-//             console.warn(`Atención: Se enviaron ${totalAInsertar} pero solo se confirmaron ${docsInsertados.length}.`);
-//         }
-//
-//     } catch (error) {
-//         console.error("Error durante la inserción:");
-//         if (error.writeErrors) {
-//             console.error(`Se insertaron ${error.insertedDocs.length} documentos, pero hubo ${error.writeErrors.length} errores.`);
-//         } else {
-//             console.error(error.message);
-//         }
-//     }
-// };
-//
-// const saveTracks = async (tracksToSave) => {
-//     try {
-//         const totalAInsertar = tracksToSave.length;
-//         const docsInsertados = await Track.insertMany(tracksToSave);
-//
-//         if (docsInsertados.length === totalAInsertar) {
-//             console.log(`Éxito total: Se insertaron los ${totalAInsertar} tracks.`);
-//         } else {
-//             console.warn(`Atención: Se enviaron ${totalAInsertar} pero solo se confirmaron ${docsInsertados.length}.`);
-//         }
-//
-//     } catch (error) {
-//         console.error("Error durante la inserción:");
-//         if (error.writeErrors) {
-//             console.error(`Se insertaron ${error.insertedDocs.length} documentos, pero hubo ${error.writeErrors.length} errores.`);
-//         } else {
-//             console.error(error.message);
-//         }
-//     }
-// };
-//
-// const saveGenres = async (genresToSave) => {
-//     try {
-//         const totalAInsertar = genresToSave.length;
-//         const docsInsertados = await Genre.insertMany(genresToSave);
-//
-//         if (docsInsertados.length === totalAInsertar) {
-//             console.log(`Éxito total: Se insertaron los ${totalAInsertar} genres.`);
-//         } else {
-//             console.warn(`Atención: Se enviaron ${totalAInsertar} pero solo se confirmaron ${docsInsertados.length}.`);
-//         }
-//
-//     } catch (error) {
-//         console.error("Error durante la inserción:");
-//         if (error.writeErrors) {
-//             console.error(`Se insertaron ${error.insertedDocs.length} documentos, pero hubo ${error.writeErrors.length} errores.`);
-//         } else {
-//             console.error(error.message);
-//         }
-//     }
-// };
+const getAllArtists = async (queryParams) => {
+    const { name, genre } = queryParams;
+    const filter = {}
+
+    if (name) filter.name = name;
+    if (genre) filter.genres = genre;
+
+    return await Artist.find(filter);
+};
+
+const getAllAlbums = async (queryParams) => {
+    const { year, genre } = queryParams;
+    const filter = {}
+
+    if (year) filter.year = year;
+    if (genre) filter.genres = genre;
+
+    return await Album.find(filter);
+};
+
+const getAlbumByArtist = async(reqParams) => {
+    const { id } = reqParams;
+    const artist = await Artist.findOne({ _id: id});
+
+    if (!artist) return [];
+
+    return await Album.find({ performer: artist.name });
+};
+
+const getAllGenres = async() => {
+    return await Genre.find({});
+};
+
+const getAllTracks = async() => {
+    return await Track.find({});
+};
 
 module.exports = {
-    // saveArtists,
-    // saveAlbums,
-    // saveTracks,
-    // saveGenres
+    getAllArtists,
+    getAllAlbums,
+    getAlbumByArtist,
+    getAllGenres,
+    getAllTracks
 };
